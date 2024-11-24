876b061ec1691ba4d365ba56cfccfbf327107acd
Siddharth Shenoy <sidshenoy2000@gmail.com>
2024-11-24 18:29:41 -0500

    Create home page of PollPulse

--- frontend/election_tweet_analysis_app/src/App.js
+++ frontend/election_tweet_analysis_app/src/App.js
@@ -1,16 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import HomePage from './components/HomePage';
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
-    return (
-        <Router>
-            <Switch>
-                <Route path='/' component={HomePage} />
-            </Switch>
-        </Router>
-    );
-};
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
 export default App;
-
