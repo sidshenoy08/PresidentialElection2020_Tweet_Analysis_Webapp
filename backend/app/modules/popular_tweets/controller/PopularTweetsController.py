747a3cdc8ae6c70205d0f96f88d77593230865e2
Siddharth Shenoy <sidshenoy2000@gmail.com>
2024-12-09 13:34:43 -0500

    Add coverage for incorrect parameters in popular_tweets module

--- backend/app/modules/popular_tweets/controller/PopularTweetsController.py
+++ backend/app/modules/popular_tweets/controller/PopularTweetsController.py
@@ -5,21 +5,45 @@ class PopularTweetsController:
 
     @staticmethod
     def get_top_tweets_by_retweets():
-        candidate = request.args.get('candidate', 'Trump')
-        limit = int(request.args.get('limit', 10))
+        try:
+            candidate = request.args.get('candidate', 'Trump')
+            if(candidate not in {"Trump", "Biden"}):
+                candidate = "Trump"
+        except:
+            candidate = "Trump"
+        try:
+            limit = int(request.args.get('limit', 10))
+        except:
+            limit = 10
         data = PopularTweetsService.get_top_tweets_by_retweets(candidate, limit)
         return jsonify(data), 200
 
     @staticmethod
     def get_top_tweets_by_likes():
-        candidate = request.args.get('candidate', 'Trump')
-        limit = int(request.args.get('limit', 10))
+        try:
+            candidate = request.args.get('candidate', 'Trump')
+            if(candidate not in {"Trump", "Biden"}):
+                candidate = "Trump"
+        except:
+            candidate = "Trump"
+        try:
+            limit = int(request.args.get('limit', 10))
+        except:
+            limit = 10
         data = PopularTweetsService.get_top_tweets_by_likes(candidate, limit)
         return jsonify(data), 200
 
     @staticmethod
     def get_tweet_location_insights():
-        candidate = request.args.get('candidate', 'Trump')
-        limit = int(request.args.get('limit', 10))
+        try:
+            candidate = request.args.get('candidate', 'Trump')
+            if(candidate not in {"Trump", "Biden"}):
+                candidate = "Trump"
+        except:
+            candidate = "Trump"
+        try:
+            limit = int(request.args.get('limit', 10))
+        except:
+            limit = 10
         data = PopularTweetsService.get_tweet_location_insights(candidate, limit)
         return jsonify(data), 200
